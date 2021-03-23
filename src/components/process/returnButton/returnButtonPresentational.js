89678bce47bffbb2027b7a8f09a85015cfdb3d9e
swing-park <ongsim123@gmail.com>
2021-03-24 01:07:20 +0900

    Add : returnButton 구현 중

--- src/components/process/returnButton/returnButtonPresentational.js
+++ src/components/process/returnButton/returnButtonPresentational.js
@@ -1,3 +1,19 @@
-class ReturnButtonPresentational { }
+class ReturnButtonPresentational {
+    constructor({ $target, moneyPocket }) {
+        // this.moneyPocket = moneyPocket;
+        // this.totalMoney = this.moneyPocket.reduce((acc, curr) => acc + curr, 0)
+        this.addEvent($target, moneyPocket)
+    }
+
+    //5초뒤 자동반환을 어떡할까..
+
+    addEvent($target, moneyPocket) {
+        $target.addEventListener("click", this.render.bind(this, moneyPocket))
+        //this.accMoney message select & setState
+    }
+
+    render(moneyPocket) {
+    }
+}
 
 export default ReturnButtonPresentational
