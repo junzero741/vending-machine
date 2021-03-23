ba61c85510bd8ce5031176a0f81b6cac2d86e67a
swing-park <ongsim123@gmail.com>
2021-03-24 01:06:11 +0900

    Add : currentMoneyContainer

--- src/components/process/currentMoneyDisplay/currentMoneyContatiner.js
+++ src/components/process/currentMoneyDisplay/currentMoneyContatiner.js
@@ -1,4 +1,15 @@
-class CurrentMoneyContainer {
+class CurrentMoneyContainer { // presentational이 되어버렸다..
+    constructor({ $target, currentMoney }) {
+        this.render($target, currentMoney)
+    }
+
+    render($target, currentMoney) {
+        const $currentMoney = `
+            <span class="currentMoney">${currentMoney} 원</span>
+        `;
+
+        $target.insertAdjacentHTML("beforeend", $currentMoney)
+    }
 
 }
 
